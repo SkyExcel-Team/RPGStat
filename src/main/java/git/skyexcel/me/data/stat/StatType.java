2064a0798fd65090889a028618b1fe8d7fc4d031
SkyExcel <inseo0121@gmail.com>
2022-09-10 10:31:14 +0900

    Changes

--- src/main/java/git/skyexcel/me/data/stat/StatType.java
+++ src/main/java/git/skyexcel/me/data/stat/StatType.java
@@ -3,6 +3,8 @@ package git.skyexcel.me.data.stat;
 public enum StatType {
     Max_Health,
     Attack_Damage, Critical_Damage,Ranged_Attack_Damage,
+
+    Defense,
     Farm,
     Mine,
     Fall,
--- src/main/java/git/skyexcel/me/event/DamageEvent.java
+++ src/main/java/git/skyexcel/me/event/DamageEvent.java
@@ -1,38 +1,89 @@
 package git.skyexcel.me.event;
 
-import org.bukkit.entity.Arrow;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.Fireball;
-import org.bukkit.entity.ThrownPotion;
+import git.skyexcel.me.data.stat.StatData;
+import git.skyexcel.me.data.stat.StatType;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.potion.Potion;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class DamageEvent implements Listener {
     @EventHandler
-    public void getDamage(EntityDamageByEntityEvent event){
+    public void getDamage(EntityDamageByEntityEvent event) {
         Entity entity = event.getEntity();
         Entity damager = event.getDamager();
         double damage = event.getDamage();
+
         EntityDamageEvent.DamageCause cause = event.getCause();
-        switch (cause){
-            case FALL :
+        switch (cause) {
+            case FALL:
 
                 break;
             case ENTITY_ATTACK:
 
-                if(event.getDamager() instanceof ThrownPotion){//Potion 데미지를 입을때
+                if (event.getDamager() instanceof ThrownPotion) {//Potion 데미지를 입을때
                     ThrownPotion potion = (ThrownPotion) event.getDamager();
+                    if (entity instanceof Player) { // 데미지를 입는 엔티티가 플레이어 일 경우
 
-                } else if(event.getDamager() instanceof Fireball){//FireBall로 데미지를 입을때
+                        Player target = (Player) damager;
+                        Player player = (Player) event.getEntity();
+                        StatData data = new StatData(player);
+
+                        double player_damage = data.addModifier(StatType.Attack_Damage).getStat();
+                        double armorPoints = target.getAttribute(Attribute.GENERIC_ARMOR).getValue();
+                        double armorToughness = target.getAttribute(Attribute.GENERIC_ARMOR_TOUGHNESS).getValue();
+
+                        PotionEffect effect = target.getPotionEffect(PotionEffectType.DAMAGE_RESISTANCE);
+                        int resistance = effect == null ? 0 : effect.getAmplifier();
+                        int epf =  getEPF(target.getInventory());
+
+                        target.damage(calculateDamageApplied(damage, armorPoints, armorToughness, resistance, epf));
+                    }
+
+                } else if (event.getDamager() instanceof Fireball) {//FireBall로 데미지를 입을때
                     ThrownPotion potion = (ThrownPotion) event.getDamager();
-                } else if(event.getDamager() instanceof Arrow){ //화살로 데미지를 입을때
+                } else if (event.getDamager() instanceof Arrow) { //화살로 데미지를 입을때
                     Arrow arrow = (Arrow) event.getDamager();
+                    if (arrow.getShooter() instanceof Player) { //플레이어가 화살로 공격을 했을 때
+                        Player player = (Player) arrow.getShooter();
+                        StatData data = new StatData(player);
+                        double player_range_damage = data.addModifier(StatType.Ranged_Attack_Damage).getStat();
+
+                    }
+
 
                 }
                 break;
         }
+    }
+    public double calculateDamageApplied(double damage, double points, double toughness,int resistance, int epf) {
+        double withArmorReduction = damage * (1 - Math.min(20, Math.max(points / 5, points - damage / (2 + toughness / 4))) / 25);
+        double withResistanceReduction = withArmorReduction * (1 - (resistance * 0.2));
+        return withResistanceReduction * (1 - (Math.min(20.0, epf) / 25));
+
+    }
+    public static int getEPF(PlayerInventory inv) {
+        ItemStack helm = inv.getHelmet();
+        ItemStack chest = inv.getChestplate();
+        ItemStack legs = inv.getLeggings();
+        ItemStack boot = inv.getBoots();
+
+        return (helm != null ? helm.getEnchantmentLevel(Enchantment.DAMAGE_ALL) : 0) +
+                (chest != null ? chest.getEnchantmentLevel(Enchantment.DAMAGE_ALL) : 0) +
+                (legs != null ? legs.getEnchantmentLevel(Enchantment.DAMAGE_ALL) : 0) +
+                (boot != null ? boot.getEnchantmentLevel(Enchantment.DAMAGE_ALL) : 0);
+    }
+    public void defense(Player player,double damage) {
+        StatData target_data = new StatData(player);
+        double defense = target_data.addModifier(StatType.Defense).getStat();
+
     }
 }
